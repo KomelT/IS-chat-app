e4d30f0a7116c87aaf5aa368c0196d0ae1663153
Tilen Komel <tilen.komel10@gmail.com>
2022-11-22 10:31:47 +0100

    dotnet new mvc

---
+++ app/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace D_real_social_app.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
