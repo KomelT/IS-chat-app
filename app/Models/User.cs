5315108374c3c3b31a37b80aa46d162f8ea203b2
Tilen Komel <tilen.komel10@gmail.com>
2022-11-23 11:46:20 +0100

    Removed some properties to use IdentityUser

--- app/Models/User.cs
+++ app/Models/User.cs
@@ -1,15 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace D_real_social_app.Models;
-public class User
+public class User : IdentityUser
 {
     public int UserID { get; set; }
     public string? Photo { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
-    public string? Email { get; set; }
-    public string? Pass { get; set; }
-    public string? Usern { get; set; }
 }
