35b3b6193c43a07b05e78d9e8c3f86d06ad46447
Tilen Komel <tilen.komel10@gmail.com>
2022-12-28 19:55:45 +0100

    Added property UserName to Feed.cs

--- app/Models/Feed.cs
+++ app/Models/Feed.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace D_real_social_app.Models;
 public class Feed
 {
+    public string? UserName { get; set; }
     public string? Text { get; set; }
     public string? PostPhoto { get; set; }
     public string? UserPhoto { get; set; }
