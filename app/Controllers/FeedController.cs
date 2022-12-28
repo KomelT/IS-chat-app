6e4a022fba2fd35ed02e1189614eefb5e760c56c
Tilen Komel <tilen.komel10@gmail.com>
2022-12-28 19:58:03 +0100

    Added image upload

--- app/Controllers/FeedController.cs
+++ app/Controllers/FeedController.cs
@@ -8,9 +8,11 @@ using Microsoft.EntityFrameworkCore;
 using D_real_social_app.Data;
 using D_real_social_app.Models;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace D_real_social_app.Controllers;
 
+[Authorize]
 public class FeedController : Controller
 {
 
@@ -26,11 +28,42 @@ public class FeedController : Controller
         var claimsIdentity = (ClaimsIdentity)User.Identity;
         var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-        ViewBag.User = userId;
+        //ViewBag.User = userId;
 
-        var sql = "SELECT id, [Post].Text AS Text, [Post].Photo AS PostPhoto, [Post].Timestamp as Timestamp, [User].Photo, [User].N AS UserPhoto FROM [Post] INNER JOIN [User] ON ([Post].UserID = [User].id) WHERE [Post].UserID = ANY ( SELECT UserID FROM Connection WHERE UserID = '" + userId + "' OR UserID2 = '" + userId + "' UNION SELECT UserID2 FROM Connection WHERE UserID = '" + userId + "' OR UserID2 = '" + userId + "') ORDER BY [Post].Timestamp DESC";
+        var sql = "SELECT [Post].Text AS Text, [Post].Photo AS PostPhoto, [Post].Timestamp as Timestamp, [User].Photo AS UserPhoto, CONCAT([User].FirstName, ' ', [User].LastName) AS UserName FROM [Post] INNER JOIN [User] ON ([Post].UserID = [User].id) WHERE [Post].UserID = '" + userId + "' ORDER BY [Post].Timestamp DESC";
         var posts = await _context.Feed.FromSqlRaw(sql).ToListAsync();
 
         return View(posts);
     }
+
+    // 
+
+    public async Task<IActionResult> uploadFile([FromForm] IFormFile file, String text)
+    {
+
+        if (text == null)
+            return BadRequest("Post Text is missing!!");
+
+        var fileName = "";
+        if (file != null)
+        {
+            fileName = "/img/" + file.FileName;
+            using (Stream fileStream = new FileStream("/app/wwwroot" + fileName, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+        }
+
+        var claimsIdentity = (ClaimsIdentity)User.Identity;
+        var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+        //var userId = "sdsadsadsadas";
+
+        var sql = "INSERT INTO [Post] (UserID, Text, Photo, Timestamp) VALUES ('" + userId + "', '" + text + "', '" + fileName + "', '" + DateTime.Now + "')";
+        _context.Database.ExecuteSqlRaw(sql);
+
+        return Redirect("/feed");
+
+    }
+
+
 }
