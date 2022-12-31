bcabd57b1db830edeeddd03851e3983833765c7d
Tilen Komel <tilen.komel10@gmail.com>
2022-12-31 09:10:16 +0100

    Image safety, random photo names, css

--- app/Controllers/FeedController.cs
+++ app/Controllers/FeedController.cs
@@ -30,7 +30,7 @@ public class FeedController : Controller
 
         //ViewBag.User = userId;
 
-        var sql = "SELECT [Post].Text AS Text, [Post].Photo AS PostPhoto, [Post].Timestamp as Timestamp, [User].Photo AS UserPhoto, CONCAT([User].FirstName, ' ', [User].LastName) AS UserName FROM [Post] INNER JOIN [User] ON ([Post].UserID = [User].id) WHERE [Post].UserID = '" + userId + "' ORDER BY [Post].Timestamp DESC";
+        var sql = "SELECT [Post].Text AS Text, [Post].Photo AS PostPhoto, [Post].Timestamp as Timestamp, [User].Photo AS UserPhoto, CONCAT([User].FirstName, ' ', [User].LastName) AS UserName FROM [Post] INNER JOIN [User] ON ([Post].UserID = [User].id) WHERE [Post].UserID = ANY ( SELECT UserID FROM Connection WHERE UserID = '" + userId + "' OR UserID2 = '" + userId + "' UNION SELECT UserID2 FROM Connection WHERE UserID = '" + userId + "' OR UserID2 = '" + userId + "' UNION SELECT '" + userId + "' FROM Connection UNION SELECT CONCAT('" + userId + "', '') AS UserId ) ORDER BY [Post].Timestamp DESC";
         var posts = await _context.Feed.FromSqlRaw(sql).ToListAsync();
 
         return View(posts);
@@ -42,12 +42,30 @@ public class FeedController : Controller
     {
 
         if (text == null)
-            return BadRequest("Post Text is missing!!");
+            return Redirect("/feed?pserr=Post%20text%20is%20missing%21");
 
         var fileName = "";
         if (file != null)
         {
-            fileName = "/img/" + file.FileName;
+            var end = file.FileName.Split(".")[file.FileName.Split(".").Length - 1];
+
+            if (!(end == "jpg" || end == "png"))
+            {
+                return Redirect("/feed?pserr=Attachment%20must%20be%20in%20.jpg%20or%20.png%20format%21");
+            }
+
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[32];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+
+            fileName = "/img/" + finalString + "." + end;
             using (Stream fileStream = new FileStream("/app/wwwroot" + fileName, FileMode.Create))
             {
                 await file.CopyToAsync(fileStream);
@@ -56,7 +74,6 @@ public class FeedController : Controller
 
         var claimsIdentity = (ClaimsIdentity)User.Identity;
         var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
-        //var userId = "sdsadsadsadas";
 
         var sql = "INSERT INTO [Post] (UserID, Text, Photo, Timestamp) VALUES ('" + userId + "', '" + text + "', '" + fileName + "', '" + DateTime.Now + "')";
         _context.Database.ExecuteSqlRaw(sql);
