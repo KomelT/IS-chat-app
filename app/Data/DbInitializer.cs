045c5d7ecb3b8d6dfb4a292d61ee4ed34287ae22
Tilen Komel <tilen.komel10@gmail.com>
2023-01-02 14:35:09 +0100

    No default data when init

--- app/Data/DbInitializer.cs
+++ app/Data/DbInitializer.cs
@@ -17,6 +17,7 @@ namespace D_real_social_app.Data
                 return;   // DB has been seeded
             }
 
+            /* 
             var users = new User[]
             {
                 new User{Photo="https://www.hostpapa.com/knowledgebase/wp-content/uploads/2018/04/1-13.png", FirstName="Lojze", LastName="Novak"},
@@ -26,7 +27,7 @@ namespace D_real_social_app.Data
             context.User.AddRange(users);
             context.SaveChanges();
 
-            /* var posts = new Post[]
+            var posts = new Post[]
              {
                  new Post{ UserID="43bc8695-e72f-43f3-aa35-5d90fbb46cdb", Text="Hello World, first post.", Photo="https://images.pexels.com/photos/9866817/pexels-photo-9866817.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", Timestamp=DateTime.Now},
                  new Post{ UserID="43bc8695-e72f-43f3-aa35-5d90fbb46cdb", Text="Hello World, post 2.", Photo="https://images.pexels.com/photos/9866817/pexels-photo-9866817.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", Timestamp=DateTime.Now},
