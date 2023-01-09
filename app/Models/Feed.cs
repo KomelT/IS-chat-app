08e19efd479b812ae40126ed3354ae7401b73cf2
Tilen Komel <tilen.komel10@gmail.com>
2023-01-09 09:18:40 +0100

    Added comments

--- app/Models/Feed.cs
+++ app/Models/Feed.cs
@@ -7,7 +7,9 @@ public class Feed
     public string? UserName { get; set; }
     public string? Text { get; set; }
     public string? PostPhoto { get; set; }
+    public int PostID { get; set; }
     public string? UserPhoto { get; set; }
     public DateTime Timestamp { get; set; }
+    public List<Comment> Comments { get; set; }
 
 }
